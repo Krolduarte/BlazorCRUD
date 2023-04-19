a60d8d8d8305aac4d4c03349ce24946c2ff0f1d3
PracticasSoftware3 <PracticasSoftware3@S-PTDES1>
2023-04-19 09:28:27 +0200

    Agregando funciones de JS e importar a Excel y CSV

---
+++ BlazorApp2/Client/Helpers/IJSExtensions.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.JSInterop;
+
+namespace BlazorApp2.Client.Helpers
+{
+    public static class IJSExtensions
+    {
+        public static ValueTask<object> GuardarComo(this IJSRuntime js, string nombreArchivo, byte[] archivo)
+        {
+            return js.InvokeAsync<object>("saveAsFile",
+                nombreArchivo,
+                Convert.ToBase64String(archivo));
+        }
+
+        public static ValueTask<object> MostrarMensaje(this IJSRuntime js, string mensaje)
+        {
+            return js.InvokeAsync<object>("Swal.fire", mensaje);
+        }
+
+        public static ValueTask<object> MostrarMensaje(this IJSRuntime js, string titulo, string mensaje, TipoMensajeSweetAlert tipoMensajeSweetAlert)
+        {
+            return js.InvokeAsync<object>("Swal.fire", titulo, mensaje, tipoMensajeSweetAlert.ToString());
+        }
+
+        public async static ValueTask<bool> Confirm(this IJSRuntime js, string titulo, string mensaje, TipoMensajeSweetAlert tipoMensajeSweetAlert)
+        {
+            return await js.InvokeAsync<bool>("CustomConfirm", titulo, mensaje, tipoMensajeSweetAlert.ToString());
+        }
+
+    }
+}
---
+++ BlazorApp2/Client/wwwroot/js/Utilidades.js
@@ -0,0 +1,10 @@
+﻿/*recibe un filename y crea un anchor dinamicamente que hace click para que se descargue el archivo*/
+
+function saveAsFile(filename, bytesBase64) {
+    var link = document.createElement('a');
+    link.download = filename;
+    link.href = "data:application/octet-strem;base64," + bytesBase64;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+}
