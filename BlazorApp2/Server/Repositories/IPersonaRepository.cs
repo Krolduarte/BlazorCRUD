60a5e2aa8728e971add39c0a746385ee3080ac1a
PracticasSoftware3 <PracticasSoftware3@S-PTDES1>
2023-04-19 15:16:33 +0200

    cON iREPOSITORY EXAMPLE

---
+++ BlazorApp2/Server/Repositories/IPersonaRepository.cs
@@ -0,0 +1,18 @@
+﻿using BlazorApp2.Shared.Models;
+
+namespace BlazorApp2.Server.Repositories
+{
+    public interface IPersonaRepository
+    {
+
+        public Task<List<Persona>> GetPersonas();
+        public Task<Persona> GetPersonaId(int id);
+
+        public Task<Persona> CrearPersona(Persona persona);
+
+        public Task<Persona> ActualizarPersona(int id, Persona persona);
+
+        public Task EliminarPersona(int id);
+
+    }
+}
---
+++ BlazorApp2/Server/Repositories/PersonaRepository.cs
@@ -0,0 +1,58 @@
+﻿using BlazorApp2.Shared.Models;
+
+namespace BlazorApp2.Server.Repositories
+{
+    public class PersonaRepository : IPersonaRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public PersonaRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<Persona> ActualizarPersona(int id, Persona persona)
+        {
+
+            var personaDesdeDb = await _context.Personas.FindAsync(id);
+
+            personaDesdeDb.Nombre = persona.Nombre;
+            personaDesdeDb.Apellido = persona.Apellido;
+            personaDesdeDb.Email = persona.Email;
+            personaDesdeDb.Descripcion = persona.Descripcion;
+            personaDesdeDb.BirthDate = persona.BirthDate;
+            personaDesdeDb.TeamId = persona.TeamId;
+            personaDesdeDb.RolId = persona.RolId;
+            personaDesdeDb.IsReadyToWork = persona.IsReadyToWork;
+
+
+            await _context.SaveChangesAsync();
+
+            return personaDesdeDb;
+
+        }
+        public async Task<Persona> CrearPersona(Persona persona)
+        {
+            if (persona is not null)
+            {
+                await _context.Personas.AddAsync(persona);
+                await _context.SaveChangesAsync();
+                return persona;
+
+            }
+        }
+
+        public Task EliminarPersona(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Persona> GetPersonaId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Persona>> GetPersonas()
+        {
+            throw new NotImplementedException();
+        }
+    }
